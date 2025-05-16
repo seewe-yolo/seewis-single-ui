ca470d5ec90f2472392fefca111b248e503ac054
xlsea <m@xlsea.cn>
2025-05-16 23:04:51 +0800

    style: 移除多余换行

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -913,21 +913,13 @@ declare namespace App {
 
     interface $T {
       (key: I18nKey): string;
-
       (key: I18nKey, plural: number, options?: TranslateOptions<LangType>): string;
-
       (key: I18nKey, defaultMsg: string, options?: TranslateOptions<I18nKey>): string;
-
       (key: I18nKey, list: unknown[], options?: TranslateOptions<I18nKey>): string;
-
       (key: I18nKey, list: unknown[], plural: number): string;
-
       (key: I18nKey, list: unknown[], defaultMsg: string): string;
-
       (key: I18nKey, named: Record<string, unknown>, options?: TranslateOptions<LangType>): string;
-
       (key: I18nKey, named: Record<string, unknown>, plural: number): string;
-
       (key: I18nKey, named: Record<string, unknown>, defaultMsg: string): string;
     }
   }
