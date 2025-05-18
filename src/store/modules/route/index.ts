c2b9ee9432d07442bcc7b4a8749c636624c9f4e2
xlsea <m@xlsea.cn>
2025-05-18 13:56:21 +0800

    fix: 修复用户信息接口调用两次问题

--- src/store/modules/route/index.ts
+++ src/store/modules/route/index.ts
@@ -387,7 +387,7 @@ export const useRouteStore = defineStore(SetupStoreId.Route, () => {
   }
 
   async function onRouteSwitchWhenLoggedIn() {
-    await authStore.initUserInfo();
+    // await authStore.initUserInfo();
   }
 
   async function onRouteSwitchWhenNotLoggedIn() {
